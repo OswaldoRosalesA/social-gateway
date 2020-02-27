531f8c8bdc9cb87687b941d262ddf05957a7e68f
Oswaldo Rosales <oswaldo.rosales.a@gmail.com>
2020-02-27 09:18:31 -0600

    Feature: Chapter 5 - Discovery Client

--- src/main/java/microservices/book/gateway/SocialGatewayApplication.java
+++ src/main/java/microservices/book/gateway/SocialGatewayApplication.java
@@ -2,9 +2,11 @@ package microservices.book.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SocialGatewayApplication {
 
